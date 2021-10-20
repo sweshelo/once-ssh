72d8c8ded422e6a6fcdac3310d79954280bc3a55
sweshelo <sweshelo@dmats97.xyz>
2021-10-20 12:33:52 +0900

    プロジェクト ファイルを追加します。

---
+++ once-ssh/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dhcpWatchClient
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("* Once SSH * Sweshelo");
+                Console.WriteLine("Usage: once-ssh host username password command");
+            }
+            else
+            {
+                string host = args[0];
+                string user = args[1];
+                string pass = args[2];
+
+                string[] commands = new string[(args.Length - 3)];
+                Array.Copy(args, 3, commands, 0, (args.Length - 3));
+
+                var sshobj = new SSHConnection(host, user, pass);
+                sshobj.Commands = commands;
+                Console.WriteLine(sshobj.Run());
+            }
+        }
+    }
+}
---
+++ once-ssh/SSH.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Renci.SshNet;
+
+namespace dhcpWatchClient
+{
+    public class SSHConnection
+    {
+        private string Host { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        public string[] Commands { get; set; }
+
+        public SSHConnection(string host, string username, string password)
+        {
+            Host = host;
+            UserName = username;
+            Password = password;
+        }
+
+        public string Run()
+        {
+            string str = "";
+
+            using (var client = new SshClient(Host, UserName, Password))
+            {
+                client.Connect();
+                if (!client.IsConnected) throw new System.Security.Authentication.AuthenticationException();
+                Array.ForEach(Commands, x => str += client.CreateCommand(x).Execute());
+                client.Disconnect();
+            }
+
+            return str;
+        }
+    }
+
+}
